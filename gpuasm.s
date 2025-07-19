f05e5a5be5f2faa9fd0fb7c2dbb0a9809ff91e83
42Bastian <bs42@monlynx.de>
2025-07-19 07:56:55 +0200

    draw_string_off: use r15 for Blitter access => save massive bytes

--- gpuasm.s
+++ gpuasm.s
@@ -66,42 +66,32 @@ _draw_string_off:
 
 		move	r0, r14
 
-		movei	#A1_BASE, r11
+		movei	#A1_BASE, r15
 
 		load	(r14+8), r0			; Get surface address in r0
 
 		movei	#$88ff88ff, r4
-
 		add	r3, r0				; Add frame offset into r0
 
-		movei	#B_PATD, r5
-		movei	#A1_CLIP, r3
-
-		store	r0, (r11)			; Store A1_BASE (destination addr)
-		store	r4, (r5)			; Store white in B_PATD low dword
-		addq	#4, r5				; Point r5 at 2nd dword of B_PATD
-		store	zeror, (r3)			; Store 0 in A1_CLIP (No clipping)
-		store	r4, (r5)			; Store white in B_PATD high dword
+		store	r0, (r15)			; Store A1_BASE (destination addr)
+		store	r4, (r15+(B_PATD-A1_BASE)/4)			; Store white in B_PATD low dword
+		store	zeror, (r15+(A1_CLIP-A1_BASE)/4); Store 0 in A1_CLIP (No clipping)
+		store	r4, (+(B_PATD-A1_BASE+4)/4)			; Store white in B_PATD high dword
 
 		movei	#XADDPIX|YADD0, r12
 		load	(r14+9), r4			; Get sprite blitter flags in r4
-		movei	#A1_FLAGS, r5
-
-		movei	#A1_FPIXEL, r7
 		or	r12, r4
 
 		; Add (-CHR_WIDTH, 1) to dst x, y pointers after each inner loop iter
 		movei	#(1<<16)|((-CHR_WIDTH)&$ffff), r8
-		movei	#A1_STEP, r9
-		movei	#A1_FSTEP, r10
 
-		store	r4, (r5)			; Store A1_FLAGS
-		store	zeror, (r7)			; Store 0 in A1_FPIXEL
-		store	r8, (r9)			; Store (-CHR_WIDTH, 1) in A1_STEP
-		store	zeror, (r10)			; Store 0 in A1_FSTEP
+		store	r4, (r15+(A1_FLAGS-A1_BASE)/4)	  ; Store A1_FLAGS
+		store	zeror, (r15+(A1_PIXEL-A1_BASE)/4) ; Store 0 in A1_FPIXEL
+		store	r8, (r15+(A1_STEP-A1_BASE)/4)			; Store (-CHR_WIDTH, 1) in A1_STEP
+		store	zeror, (r15+(A1_FSTEP-A1_BASE)/4)			; Store 0 in A1_FSTEP
 
 		movei	#fontdata, r4
-		movei	#A2_BASE, r5
+
 		; Use PIXEL8 even though we're reading 1bit pixels. This, combined
 		; with the below SRCENX + !SRCEN command, FNTWIDTH set to actual
 		; divided by 8, fixed A2_STEP of -1, 1, and blit width of at most 8
@@ -115,17 +105,13 @@ _draw_string_off:
 		; clamped to 8 bits/1 byte given source data is only read once. This
 		; case is not implemented here.
 		movei	#PITCH1|PIXEL8|FNTWIDTH|XADDPIX|YADD0, r0
-		movei	#A2_FLAGS, r7
 		movei	#(1<<16)|((-1)&$ffff), r8
-		movei	#A2_STEP, r9
 
-		store	r4, (r5)			; Store fontdata in A2_BASE
-		store	r0, (r7)			; Store A2_FLAGS
-		store	r8, (r9)			; Store (-1, 1) in A2_STEP
+		store	r4, (r15+(A2_BASE-A1_BASE)/4)	; Store fontdata in A2_BASE
+		store	r0, (r15+(A2_FLAGS-A1_BASE)/4)			; Store A2_FLAGS
+		store	r8, (r15+(A2_STEP-A1_BASE)/4)			; Store (-1, 1) in A2_STEP
 
-		movei	#A1_PIXEL, r12
 		movei	#(CHR_HEIGHT<<16)|CHR_WIDTH, r3
-		movei	#B_COUNT, r4
 
 		; The blit loop always starts by adding CHR_WIDTH to the dst
 		; position. Subtract CHR_WIDTH from the initial position to account
@@ -138,38 +124,38 @@ _draw_string_off:
 		;  -!SRCEN means no pixels read on other inner loop iterations
 		;  -BCOMPEN uses bitmask to decide which pixels to actually write
 		movei	#SRCENX|DSTEN|UPDA1|UPDA2|PATDSEL|BCOMPEN, r5;
-		movei	#B_CMD, r0
+
 		movei	#FNTFIRSTCHR, r8	; Load first char idx of font in r8
 		movei	#FNTLASTCHR, r9		; Load last char idx of font in r9
-		movei	#A2_PIXEL, r10
 
-		jr		.nextchr			; Jump into loop
-		xor		r11, r11			; clear r11
+		jr	.nextchr		; Jump into loop
+		xor	r11, r11		; clear r11
 
-.blitloop:	store	r7, (r10)			; Store src pixel loc in A2_PIXEL
-		store	r1, (r12)			; Store dst pixel loc in A1_PIXEL
-		store	r3, (r4)			; Write loop dimensions to B_COUNT
-		store	r5, (r0)			; Write op to B_CMD
+.blitloop:	store	r7, (r15+(A2_PIXEL-A1_BASE)/4)			; Store src pixel loc in A2_PIXEL
+		store	r1, (r15+(A1_PIXEL-A1_BASE)/4)			; Store dst pixel loc in A1_PIXEL
+		store	r3, (r15+(B_COUNT-A1_BASE)/4)			; Write loop dimensions to B_COUNT
+		store	r5, (r15+(B_CMD-A1_BASE)/4)			; Write op to B_CMD
 
-.nextchr:	addq	#CHR_WIDTH, r1		; Add CHR_WIDTH to dst pixel location
-		loadb	(r2), r7			; Load next character
-		cmpq	#0, r7				; At NUL terminator?
-		jr		EQ, .waitlast		; if yes, wait for the last blit
-		cmp		r9, r7				; Compare to last char
-		addqt	#1, r2				; Always advance string pointer
-		jr		HI, .nextchr		; If chr out of range, leave blank space
-		sub		r8, r7				; Subtract font first chr from character
-		jr		MI, .nextchr		; If chr out of range, leave blank space
+.nextchr:
+		loadb	(r2), r7		; Load next character
+		cmpq	#0, r7			; At NUL terminator?
+		addqt	#CHR_WIDTH, r1		; Add CHR_WIDTH to dst pixel location
+		jr	EQ, .waitlast		; if yes, wait for the last blit
+		cmp	r9, r7			; Compare to last char
+		addqt	#1, r2			; Always advance string pointer
+		jr	HI, .nextchr		; If chr out of range, leave blank space
+		sub	r8, r7			; Subtract font first chr from character
+		jr	MI, .nextchr		; If chr out of range, leave blank space
 		nop
-.waitblit:	btst	#0, r11				; See if bit 0 is set
-		jr		NE, .blitloop		; If done, next iteration
-		xor		r11, r11			; Clear r11 for next iteration
-		jr		.waitblit			; Else, keep waiting
+.waitblit:	btst	#0, r11			; See if bit 0 is set
+		jr	NE, .blitloop		; If done, next iteration
+		xor	r11, r11		; Clear r11 for next iteration
+		jr	.waitblit		; Else, keep waiting
 
 .waitlast:	; Done. Wait for the last blit...
-		load	(r0), r11			; In both loops: Read back blit status
+		load	(r15+(B_CMD-A1_BASE)/4), r11	; In both loops: Read back blit status
 		btst	#0, r11				; See if bit 0 is set
-		jr		EQ, .waitlast
+		jr	EQ, .waitlast
 		nop					; Don't signal completion while spinning
 
 .done:		; Return to the caller.
